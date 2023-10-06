27d3a414fdae45098d3a9dd108e9e0ade5fe41a7
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-05 20:00:20 -0400

    Create setup3.py

---
+++ setup3.py
@@ -0,0 +1,18 @@
+from paramiko import SSHClient, AutoAddPolicy
+from rich import print, pretty, inspect
+
+client = SSHClient()
+
+client.load_system_host_keys()
+
+client.set_missing_host_key_policy(AutoAddPolicy())
+
+client.connect("54.224.191.185", username="ubuntu")
+
+stdin, stdout, stderr = client.exec_command('''
+                                            cd c4_deployment-5
+                                            
+                                            ''')
+
+#print(stdout.read().decode("utf-8"))
+print(stderr.read().decode("utf-8"))

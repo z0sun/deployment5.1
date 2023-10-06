8ae17ad22b783d706e9f7f53b6b7ec72856f8b8d
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-05 20:49:34 -0400

    Update setup3.py

--- setup3.py
+++ setup3.py
@@ -11,7 +11,9 @@ client.connect("54.224.191.185", username="ubuntu")
 
 stdin, stdout, stderr = client.exec_command('''
                                             source test/bin/activate
-                                            cd c4_deployment-5 
+                                            rm -rf c4_deployment-5
+                                            git clone https://github.com/kura-labs-org/c4_deployment-5.git
+                                            cd c4_deployment-5
                                             python -m gunicorn app:app -b 0.0.0.0 -D
                                             echo "Done"
                                             ''')

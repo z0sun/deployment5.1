9ba23b27be7706fbbfc884b8088424edd5600533
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-05 20:39:34 -0400

    Update setup2.py

--- setup2.py
+++ setup2.py
@@ -7,13 +7,14 @@ client.load_system_host_keys()
 
 client.set_missing_host_key_policy(AutoAddPolicy())
 
-client.connect("54.224.42.177", username="ubuntu")
+client.connect("54.224.191.185", username="ubuntu")
 
-stdin, stdout, stderr = client.exec_command('''cd c4_deployment-5
+stdin, stdout, stderr = client.exec_command('''
+                                            cd c4_deployment-5
                                             chmod 700 Pkill.sh
                                             ./Pkill.sh
-                                            python -m gunicorn app:app -b 0.0.0.0 --deamon
                                             ''')
 
-#print(stdout.read().decode("utf-8"))
+print(stdout.read().decode("utf-8"))
 print(stderr.read().decode("utf-8"))
+client.close()
